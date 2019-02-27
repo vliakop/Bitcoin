1dd0690c56c2a37f43500cd25ea2b3dad308841a
vliakop <sdi1100097@di.uoa.gr>
2019-02-27 15:37:27 +0200

    Added a BITCOINLIST:  > Pending Testing  > Pending Completion

---
+++ classes/Lists/BitcoinList.cpp
@@ -0,0 +1,68 @@
+#include "BitcoinList.h"
+
+BitcoinList::BitcoinNode::BitcoinNode(char *bitcoin_id, int bitcoin_value, int denomination,
+                                      BitcoinList::BitcoinNode *previous, BitcoinList::BitcoinNode *next) {
+    bitcoin = new Bitcoin(bitcoin_id, bitcoin_value, denomination);
+    this->previous = previous;
+    this->next = next;
+}
+
+BitcoinList::BitcoinNode::~BitcoinNode() {
+
+    delete bitcoin;
+}
+
+BitcoinList::BitcoinList() {
+
+    head = tail = NULL;
+    size = 0;
+}
+
+BitcoinList::~BitcoinList() {
+
+    BitcoinList::BitcoinNode *n;
+    n = head;
+    while (n != NULL) {
+        head = head->next;
+        delete n;
+        n = head;
+    }
+}
+
+BitcoinList::BitcoinNode *BitcoinList::getHead() const {
+    return head;
+}
+
+void BitcoinList::setHead(BitcoinList::BitcoinNode *head) {
+    this->head = head;
+}
+
+BitcoinList::BitcoinNode *BitcoinList::getTail() const {
+    return tail;
+}
+
+void BitcoinList::setTail(BitcoinList::BitcoinNode *tail) {
+    this->tail = tail;
+}
+
+int BitcoinList::getSize() const {
+    return size;
+}
+
+void BitcoinList::setSize(int size) {
+    this->size = size;
+}
+
+bool BitcoinList::add(char *bitcoin_id, int bitcoin_value, int denomination) {
+
+    if (size == 0) {
+        head = tail = new BitcoinList::BitcoinNode(bitcoin_id, bitcoin_value, denomination);
+        size += 1;
+    } else {
+        BitcoinList::BitcoinNode *n = new BitcoinList::BitcoinNode(bitcoin_id, bitcoin_value, denomination);
+        tail->next = n;
+        tail = n;
+    }
+    size += 1;
+    return true;
+}
---
+++ classes/Lists/BitcoinList.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <cstddef>
+#include "../Bitcoin.h"
+
+class BitcoinList {
+
+private:
+
+    struct BitcoinNode {
+
+        Bitcoin *bitcoin;
+        BitcoinNode *previous;
+        BitcoinNode *next;
+
+        BitcoinNode(char *bitcoin_id, int bitcoin_value, int denomination, BitcoinNode *previous = NULL, BitcoinNode *next = NULL);
+        ~BitcoinNode();
+    };
+
+    BitcoinNode *head;
+    BitcoinNode *tail;
+    int size;
+
+public:
+
+    BitcoinList();
+    ~BitcoinList();
+
+    BitcoinNode *getHead() const;
+
+    void setHead(BitcoinNode *head);
+
+    BitcoinNode *getTail() const;
+
+    void setTail(BitcoinNode *tail);
+
+    int getSize() const;
+
+    void setSize(int size);
+
+    bool add(char *bitcoin_id, int bitcoin_value, int denomination);
+
+//    Bitcoin* removeFromStart();
+//
+//    Bitcoin* removeFromEnd();
+//
+//    Bitcoin* removeBitcoinWithId(int id);
+//
+//    void print();
+
+
+
+};
+
+
+
