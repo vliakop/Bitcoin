c3a6f2ff06164a3dda33bfff5aa877166f7f0c6a
vliakop <sdi1100097@di.uoa.gr>
2019-03-03 23:44:28 +0200

    Added TransactionList class:  > Added the source files

---
+++ classes/Lists/TransactionList.cpp
@@ -0,0 +1,68 @@
+#include "TransactionList.h"
+
+TransactionList::TransactionNode::TransactionNode(Transaction *transaction, TransactionList::TransactionNode *next) {
+
+    this->transaction = transaction;
+    this->next = next;
+}
+
+TransactionList::TransactionNode::~TransactionNode() {
+
+    if (transaction != NULL) {
+        delete transaction;
+    }
+}
+
+TransactionList::TransactionList() {
+
+    head = tail = NULL;
+    size = 0;
+}
+
+TransactionList::~TransactionList() {
+
+    TransactionNode *n = head;
+    while (head != NULL) {
+        head = head->next;
+        delete n;
+        n = head;
+        size--;
+    }
+}
+
+TransactionList::TransactionNode *TransactionList::getHead() const {
+    return head;
+}
+
+void TransactionList::setHead(TransactionList::TransactionNode *head) {
+    TransactionList::head = head;
+}
+
+TransactionList::TransactionNode *TransactionList::getTail() const {
+    return tail;
+}
+
+void TransactionList::setTail(TransactionList::TransactionNode *tail) {
+    TransactionList::tail = tail;
+}
+
+int TransactionList::getSize() const {
+    return size;
+}
+
+void TransactionList::setSize(int size) {
+    TransactionList::size = size;
+}
+
+
+void TransactionList::add(Transaction *transaction) {
+
+    if (size == 0) {
+        head = tail = new TransactionList::TransactionNode(transaction);
+    } else {
+        TransactionNode *n = new TransactionNode(transaction);
+        tail->next = n;
+        tail = n;
+    }
+    size += 1;
+}
---
+++ classes/Lists/TransactionList.h
@@ -0,0 +1,42 @@
+#pragma once
+
+
+#include "../Transaction.h"
+
+class TransactionList {
+
+private:
+
+    struct TransactionNode{
+
+        Transaction *transaction;
+        TransactionNode *next;
+
+        TransactionNode(Transaction *transaction, TransactionNode *next = NULL);
+        ~TransactionNode();
+    };
+
+    TransactionNode *head;
+    TransactionNode *tail;
+    int size;
+
+public:
+
+    TransactionList();
+    ~TransactionList();
+
+    TransactionNode *getHead() const;
+
+    void setHead(TransactionNode *head);
+
+    TransactionNode *getTail() const;
+
+    void setTail(TransactionNode *tail);
+
+    int getSize() const;
+
+    void setSize(int size);
+
+    void add(Transaction *transaction);
+};
+
