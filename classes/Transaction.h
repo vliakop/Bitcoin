14dc68cda765e18570c7165b9934ce78f3bf5a38
vliakop <sdi1100097@di.uoa.gr>
2019-03-03 18:52:36 +0200

    Completion to previous commit

--- classes/Transaction.h
+++ classes/Transaction.h
@@ -1,7 +1,35 @@
 #pragma once
 
+#include <ctime>
 
 class Transaction {
+
+private:
+
+    char transactionID[15];
+    char senderWalletID[50];
+    char receiverWalletID[50];
+    int value;
+    struct tm tm;
+
+public:
+
+    Transaction(char *transactionID, char *senderWalletID, char *receiverWalletID, int value, char *date);
+
+    ~Transaction();
+
+    char* getTransactionID();
+    void setTransactionID(char *transactionID);
+    char *getSenderWalletID();
+    void setSenderWalletID(char *senderWalletID);
+    char *getReceiverWalletID();
+    void setReceiverWalletID(char *receiverWalletID);
+    int getValue();
+    void setValue(int value);
+    struct tm getDate();
+    void setDate(char *date);
+    void dateToString();
+    void print();
 
 };
 
